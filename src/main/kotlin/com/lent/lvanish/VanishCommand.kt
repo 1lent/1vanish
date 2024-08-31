b242ce8b74c03d2a6d5c871ee6237372f3198c4c
1lent <brodiecuff@outlook.com>
2024-08-31 16:18:00 +0800

    Initial commita

---
+++ src/main/kotlin/com/lent/lvanish/VanishCommand.kt
@@ -0,0 +1,4 @@
+package com.lent.Lvanish
+
+class VanishCommand {
+}
---
+++ src/main/kotlin/com/lent/lvanish/VanishManager.kt
@@ -0,0 +1,7 @@
+package com.lent.lvanish
+
+import java.util.*
+
+object VanishManager {
+    val vanished: MutableSet<UUID> = HashSet()
+}
